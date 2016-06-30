4f991509d349353c098961ee542eecdfff442380
davidlmobley <dmobley@gmail.com>
2016-06-29 22:24:59 -0700

    Add tracking of which base types are used vs unused.

--- smarty/sampler.py
+++ smarty/sampler.py
@@ -102,6 +102,12 @@ class AtomTypeSampler(object):
         # Store smarts for basetypes
         self.basetypes_smarts = [ smarts for (smarts, name) in self.basetypes ]
 
+        # Store initially populated base types, as these will be retained even 
+        # if they have zero occupancy (whereas unpopulated base types
+        # need never be used ever and can be deleted- i.e. if we have no 
+        # phosphorous in the set we don't need a phosphorous base type)
+        self.used_basetypes = []
+
         # Store a deep copy of the molecules since they will be annotated
         self.molecules = copy.deepcopy(molecules)
 
@@ -139,10 +145,21 @@ class AtomTypeSampler(object):
                 for atom in molecule.GetAtoms():
                     atomtype = atom.GetType()
                     self.reference_atomtypes_atomcount[atomtype] += 1
-
+        
         # Maintain a list of SMARTS matches without any atom type matches in the dataset
         # This is used for efficiency.
         self.atomtypes_with_no_matches = set()
+        
+        # Track used vs unused base types
+        for (smarts, atom_type) in self.basetypes:
+            # If this type is used, then track it
+            if atom_typecounts[atom_type] > 0:
+                self.used_basetypes.append( [ smarts, atom_type] )
+                if self.verbose: print("Storing used base type `%s`, name `%s` with count %s..." % (smarts, atom_type, atom_typecounts[atom_type] )) 
+            # If unused, it matches nothing in the set
+            else:  
+                self.atomtypes_with_no_matches.add( smarts )
+                if self.verbose: print("Storing atom type `%s`, which is unused, so that it will not be tested further." % smarts )   
 
         return
 
