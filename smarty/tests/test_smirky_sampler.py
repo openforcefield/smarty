7ce9e42efefe1107bdabcb8346da9036f5291944
Caitlin Bannan <bannanc@uci.edu>
2016-08-16 15:10:07 -0700

    fix tests for smirky sampler and initiate sampler with SMIRKS intead of chemical environments

--- smarty/tests/test_smirky_sampler.py
+++ smarty/tests/test_smirky_sampler.py
@@ -1,3 +1,4 @@
+from unittest import TestCase
 from smarty.environment import *
 from smarty.sampler_smirky import *
 from smarty.utils import *
@@ -5,27 +6,23 @@ from operator import itemgetter, attrgetter
 from openeye.oechem import *
 import copy
 import sys # used to exit while testing
+# General things
+ORs = ['X1', 'X2', 'X3', 'X4']
+ANDs = ['+0']
+mol2file = 'molecules/test_filt1_tripos.mol2'
+SMIRFF = "forcefield/Frosst_AlkEtOH.ffxml"
+elements = ["[#%i]" %i for i in range(1,119)]
+replacements = None
+molecules = read_molecules(get_data_filename(mol2file), verbose = False)
 
 class TestSmirkySampler(TestCase):
-    
-    def __init__(self):
-        # General things
-        self.ORs = ['X1', 'X2', 'X3', 'X4']
-        self.ANDs = ['+0']
-        self.mol2file = 'test_filt1_tripos.mol2'
-        self.SMIRFF = "forcefield/Frosst_AlkEtOH.ffxml"
-        self.verbose = True
-        self.elements = ["[#%i]" %i for i in range(1,119)]
-        self.replacements = None
-        self.molecules = read_molecules(mol2file, verbose = verbose)
-
     def test_correctVdW(self):
         """
         Test 100% score with correct VdW types in AlkEtOH
         """
         typetag = 'VdW'
         
-        initialList = [ ["[#1:1]-[#6]]", 'HC'], 
+        initialList = [ ["[#1:1]-[#6]", 'HC'], 
                 [ "[#1:1]-[#6]-[#7,#8,F,#16,Cl,Br]", 'H1'],
                 [ "[#1:1]-[#6](-[#7,#8,F,#16,Cl,Br])-[#7,#8,F,#16,Cl,Br]", 'H2'],
                 [ "[#1:1]-[#6](-[#7,#8,F,#16,Cl,Br])(-[#7,#8,F,#16,Cl,Br])-[#7,#8,F,#16,Cl,Br]", 'H3'],
@@ -34,7 +31,7 @@ class TestSmirkySampler(TestCase):
                 [ "[#8X2:1]", 'OS'],
                 [ "[#8X2+0:1]-[#1]", 'OH'] ]
 
-        sampler = TypeSampler(self.molecules, typetag, self.elements, self.ORs, self.ANDs, self.replacements, initialList, self.SMIRFF, 0.0, False)
+        sampler = TypeSampler(molecules, typetag, elements, ORs, ANDs, replacements, initialList, SMIRFF, 0.0, False)
 
         fracfound = sampler.run(2)
         if fracfound < 1.0:
